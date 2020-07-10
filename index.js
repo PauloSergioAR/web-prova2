8839a9bf22ce4859742168c356af4fea782ce307
Paulo Sérgio Aquino Ribeiro <paulo.sergio.ar@gmail.com>
2020-07-09 22:18:08 -0300

    sla
    Signed-off-by: Paulo Sérgio Aquino Ribeiro <paulo.sergio.ar@gmail.com>

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 let inputs = Array.from(document.getElementsByClassName("in"))
 
 inputs.forEach((element) => {
-  element.addEventListener('click', function(e) {
-    alert(this.id)
+  element.addEventListener('blur', function() {
+    input = document.getElementById(this.id)
+
+    value = input.value
+    console.log(value)
+    if(value.charAt(0) == "="){
+      value = value.substring(1)
+
+      console.log(value.split("+"))
+      if(value.split("+".length) > 0){
+        input.value = (parseInt(document.getElementById(value.split("+")[0]))) + (parseInt(document.getElementById(value.split("+")[1])))
+      } else if (value.split("-".length) > 0){
+        input.value = (parseInt(document.getElementById(value.split("-")[0]))) - (parseInt(document.getElementById(value.split("-")[1])))
+      }
+    }
   })
 })
