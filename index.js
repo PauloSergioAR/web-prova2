2e6336266dc763455a71b68651c23b6da9c7b07b
Paulo Sérgio Aquino Ribeiro <paulo.sergio.ar@gmail.com>
2020-07-09 22:21:29 -0300

    Putz agora ja n adianta mais
    Signed-off-by: Paulo Sérgio Aquino Ribeiro <paulo.sergio.ar@gmail.com>

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ inputs.forEach((element) => {
     input = document.getElementById(this.id)
 
     value = input.value
-    console.log(value)
     if(value.charAt(0) == "="){
       value = value.substring(1)
 
       console.log(value.split("+"))
-      if(value.split("+".length) > 0){
+      if(value.split("+").length > 0){
         input.value = (parseInt(document.getElementById(value.split("+")[0]))) + (parseInt(document.getElementById(value.split("+")[1])))
-      } else if (value.split("-".length) > 0){
+        console.log((parseInt(document.getElementById(value.split("+")[0]))) + (parseInt(document.getElementById(value.split("+")[1]))))
+      } else if (value.split("-").length > 0){
         input.value = (parseInt(document.getElementById(value.split("-")[0]))) - (parseInt(document.getElementById(value.split("-")[1])))
       }
     }
